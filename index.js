1de6aa8d3dcc10253f8d12ef80a195402acf7841
Huynh Quoc Huy <huykyunh.k@gmail.com>
2025-06-27 00:17:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var chudeRouter = require('./routers/chude');
 var taikhoanRouter = require('./routers/taikhoan');
 var baivietRouter = require('./routers/baiviet');
 
-var uri = 'mongodb+srv://huykyunhk:HuyKyunH.K07@cluster0.17bgl.mongodb.net/trangtin';
+var uri = '<MY-URI>';
 mongoose.connect(uri).catch(err => console.log(err));
 
 app.set('views', './views');
